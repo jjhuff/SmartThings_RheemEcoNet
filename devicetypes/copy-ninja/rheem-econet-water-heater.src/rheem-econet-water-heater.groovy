cde7167ee99fda32d06dd706818b91f12f715aad
Justin Huff <jjhuff@mspin.net>
2017-01-01 09:56:40 -0800

    Convert to new Rheem API

--- devicetypes/copy-ninja/rheem-econet-water-heater.src/rheem-econet-water-heater.groovy
+++ devicetypes/copy-ninja/rheem-econet-water-heater.src/rheem-econet-water-heater.groovy
@@ -68,107 +68,46 @@ metadata {
 
 def parse(String description) { }
 
-def poll() { updateDeviceData(parent.getDeviceData(this.device)) }
+//def poll() {
+	//updateDeviceData(parent.getDeviceData(this.device))
+//}
 
-def refresh() { parent.refresh() }
+def refresh() {
+	log.debug "refresh"
+	parent.refresh()
+}
 
 def on() {
-	def deviceData = parent.getDeviceData(this.device).clone()
-    deviceData.enabled = true
-   	state.deviceData = deviceData
-	runIn(5, setDeviceMode, [overwrite: true])
-}
-def off() {
-	def deviceData = parent.getDeviceData(this.device).clone()
-    deviceData.enabled = false
-   	state.deviceData = deviceData
-	runIn(5, setDeviceMode, [overwrite: true])
+   	parent.setDeviceEnabled(this.device, true)
+    sendEvent(name: "switch", value: "off")
 }
 
-def setHeatingSetpoint(Number heatingSetPoint) {
-	def actualData = parent.getDeviceData(this.device).clone()
-    def deviceData = convertTemperatureUnit(actualData.clone(), getTemperatureScale())
-    
-	heatingSetPoint = (heatingSetPoint < deviceData.minTemp)? deviceData.minTemp : heatingSetPoint
+def off() {
+   	parent.setDeviceEnabled(this.device, false)
+    sendEvent(name: "switch", value: "off")
+}
+
+def setSetPoint(Number setPoint) {
+	/*heatingSetPoint = (heatingSetPoint < deviceData.minTemp)? deviceData.minTemp : heatingSetPoint
 	heatingSetPoint = (heatingSetPoint > deviceData.maxTemp)? deviceData.maxTemp : heatingSetPoint
-	deviceData.setPoint = heatingSetPoint
-    
-	updateDeviceData(deviceData)    
-	
-	state.deviceData = convertTemperatureUnit(deviceData, actualData.temperatureUnit)
-	runIn(5, setDeviceSetPoint, [overwrite: true])
+    */
+   	sendEvent(name: "heatingSetpoint", value: setPoint, unit: "F")
+	parent.setDeviceSetPoint(this.device, setPoint)
 }
 
 def heatLevelUp() { 
-	def actualData = parent.getDeviceData(this.device).clone()
-	def deviceData = convertTemperatureUnit(actualData.clone(), getTemperatureScale())
-	def heatingSetPoint = device.currentValue("heatingSetpoint")
-	if (actualData.temperatureUnit != getTemperatureScale()) {
-		if (actualData.temperatureUnit == "C") {
-			actualData.setPoint = Math.round(fahrenheitToCelsius(heatingSetPoint)).toInteger()
-			actualData.setPoint = ((actualData.setPoint + 1) > actualData.maxTemp)? actualData.maxTemp : (actualData.setPoint + 1)
-			heatingSetPoint = Math.round(celsiusToFahrenheit(actualData.setPoint)).toInteger()
-		} else {
-			heatingSetPoint = ((heatingSetPoint + 1) > deviceData.maxTemp)? deviceData.maxTemp : (heatingSetPoint + 1)
-			actualData.setPoint = Math.round(celsiusToFahrenheit(heatingSetPoint)).toInteger()
-		}
-	} else {
-		heatingSetPoint = ((heatingSetPoint + 1) > deviceData.maxTemp)? deviceData.maxTemp : (heatingSetPoint + 1)
-		actualData.setPoint = heatingSetPoint
-	}
-	deviceData.setPoint = heatingSetPoint
-	
-	updateDeviceData(deviceData) 
-	setHeatingSetpoint(heatingSetPoint) 
+	def setPoint = device.currentValue("heatingSetpoint")
+    setPoint = setPoint + 1
+	setSetPoint(setPoint)
 }	
 
 def heatLevelDown() { 
-	def actualData = parent.getDeviceData(this.device).clone()
-	def deviceData = convertTemperatureUnit(actualData.clone(), getTemperatureScale())
-	def heatingSetPoint = device.currentValue("heatingSetpoint")
-	if (actualData.temperatureUnit != getTemperatureScale()) {
-		if (actualData.temperatureUnit == "C") {
-			actualData.setPoint = Math.round(fahrenheitToCelsius(heatingSetPoint)).toInteger()
-			actualData.setPoint = ((actualData.setPoint - 1) < actualData.minTemp)? actualData.minTemp : (actualData.setPoint - 1)
-			heatingSetPoint = Math.round(celsiusToFahrenheit(actualData.setPoint)).toInteger()
-		} else {
-			heatingSetPoint = ((heatingSetPoint - 1) < deviceData.minTemp)? deviceData.minTemp : (heatingSetPoint - 1)
-			actualData.setPoint = Math.round(celsiusToFahrenheit(heatingSetPoint)).toInteger()
-		}
-	} else {
-		heatingSetPoint = ((heatingSetPoint - 1) < deviceData.minTemp)? deviceData.minTemp : (heatingSetPoint - 1)
-		actualData.setPoint = heatingSetPoint
-	}
-	deviceData.setPoint = heatingSetPoint
-	
-	updateDeviceData(deviceData) 
-	setHeatingSetpoint(heatingSetPoint) 
+	def setPoint = device.currentValue("heatingSetpoint")
+    setPoint = setPoint - 1
+    setSetPoint(setPoint)
 }
 
-def updateDeviceData(actualData) {
-	def deviceData = convertTemperatureUnit(actualData, getTemperatureScale())
-	sendEvent(name: "heatingSetpoint", value: deviceData.setPoint, unit: getTemperatureScale())
-    //log.info("updateDeviceData: ${deviceData.enabled} ${deviceData.mode}")
-    sendEvent(name: "switch", value: deviceData.enabled ? "on" : "off")
+def updateDeviceData(data) {
+	sendEvent(name: "heatingSetpoint", value: data.setPoint, unit: "F")
+    sendEvent(name: "switch", value: data.isEnabled ? "on" : "off")
 }
-
-def convertTemperatureUnit(actualData = [], temperatureUnit) {
-	def deviceData = actualData.clone()
-	if (deviceData.temperatureUnit != temperatureUnit) { 
-		if (deviceData.temperatureUnit == "F") {
-			deviceData.temperatureUnit = "C"
-			deviceData.setPoint = Math.round(fahrenheitToCelsius(deviceData.setPoint)).toInteger() as Integer
-			deviceData.maxTemp = Math.round(fahrenheitToCelsius(deviceData.maxTemp)).toInteger() as Integer
-			deviceData.minTemp = Math.round(fahrenheitToCelsius(deviceData.minTemp)).toInteger() as Integer
-		} else {
-			deviceData.temperatureUnit = "F"
-			deviceData.setPoint = Math.round(celsiusToFahrenheit(deviceData.setPoint)).toInteger() as Integer
-			deviceData.maxTemp = Math.round(celsiusToFahrenheit(deviceData.maxTemp)).toInteger() as Integer
-			deviceData.minTemp = Math.round(celsiusToFahrenheit(deviceData.minTemp)).toInteger() as Integer
-		}
-	}
-	return deviceData
-}
-
-def setDeviceSetPoint() { parent.setDeviceSetPoint(this.device, state.deviceData) }
-def setDeviceMode() { parent.setDeviceMode(this.device, state.deviceData) }
