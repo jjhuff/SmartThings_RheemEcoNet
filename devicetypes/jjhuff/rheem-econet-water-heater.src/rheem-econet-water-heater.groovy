3932aac78e4b294785efa10d9003ac96499f65b9
Justin Huff <jjhuff@mspin.net>
2017-01-04 08:39:50 -0800

    Update modified date

--- devicetypes/jjhuff/rheem-econet-water-heater.src/rheem-econet-water-heater.groovy
+++ devicetypes/jjhuff/rheem-econet-water-heater.src/rheem-econet-water-heater.groovy
@@ -12,7 +12,7 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
- *  Last Updated : 1/1/17
+ *  Last Updated : 2017-01-04
  *
  *  Based on https://github.com/copy-ninja/SmartThings_RheemEcoNet
  */
